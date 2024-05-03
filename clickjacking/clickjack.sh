67996c198b4c06d3656f7b6a52267e12aa2b6148
Ameer <114929844+Ameer-clk@users.noreply.github.com>
2024-05-03 07:41:17 +0000

    banner modifiacation

--- clickjacking/clickjack.sh
+++ clickjacking/clickjack.sh
@@ -23,7 +23,7 @@ echo -e ${CP}"| |___| | | (__|   <  | |_| | (_| | (__|   <| | | | | (_| |       
 echo -e ${CP}" \____|_|_|\___|_|\_\  \___/ \__,_|\___|_|\_\_|_| |_|\__, |                    #"
 echo -e ${CP}"                                                     |___/                     #"
 echo -e ${CNC}"       A Simple Tool To Find ClickJacking Vulnerability With POC              #"
-echo -e ${GREEN}"                     Coded By: Ameer Ali                                   #"
+echo -e ${GREEN}"                     Coded By: Ameer Ali                                    #"
 echo -e ${CP}"         Follow me: Github: https://github.com/Ameer-clk                       #"
 echo -e ${CP}"                                                                               #"
 echo -e ${RED}"#############################################################################\n "
